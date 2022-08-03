77aadc0c8630b837edcbef8a73e66605aed33d7f
Rob Webster <robwebster@mailbox.org>
2022-08-03 09:16:19 +0100

    Updated pH sensor data: {datetime.now().strftime("%d-%m-%Y-%H-%M-%S")

--- create_dummy_data.py
+++ create_dummy_data.py
@@ -9,7 +9,7 @@ REPO_DIR="/home/pi/dummy_sensor_data"
 
 # Append latest "data"
 with open(f'{REPO_DIR}/ph.csv', 'a+') as f:
-    f.write(f'{datetime.now().strftime("%d-%m-%Y-%H-%M-%S")},{random.randint(30,100)/10}')
+    f.write(f'{datetime.now().strftime("%d-%m-%Y-%H-%M-%S")},{random.randint(30,100)/10}\n')
 
 # Add, commit, push
 repo = Repo(REPO_DIR)
