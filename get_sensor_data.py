8002a8425b6d133892825a5f33ee69e29cea2e24
Rob Webster <robwebster@mailbox.org>
2022-08-04 12:49:39 +0100

    improve polling script to grab EC data from device and write to CSV

--- get_sensor_data.py
+++ get_sensor_data.py
@@ -6,35 +6,39 @@ import fcntl
 import time
 import copy
 import string
+from datetime import datetime
 
 sys.path.append('/home/pi/Raspberry-Pi-sample-code')
 
 from AtlasI2C import (
 	 AtlasI2C
 )
+
+address_to_name={
+    99: 'pH',
+    100: 'EC',
+    101: 'RTD',
+    102: 'PMP102',
+    103: 'PMP103',
+    104: 'PMP104',
+    105: 'PMP105'
+}
     
 def get_devices():
     device = AtlasI2C()
     device_address_list = device.list_i2c_devices()
     device_list = []
-    
-    # pH 99
-    # EC 100
-    # RTD 101
-    # PMP 102
-    # PMP 103
-    # PMP 104
-    # PMP 105
-    
-    
+        
     for i in device_address_list:
+        if i != 100: # restrict operation to only EC for now
+            continue
         device.set_i2c_address(i)
         response = device.query("I")
         try:
             moduletype = response.split(",")[1] 
             response = device.query("name,?").split(",")[1]
         except IndexError:
-            print(">> WARNING: device at I2C address " + str(i) + " has not been identified as an EZO device, and will not be queried") 
+            #print(">> WARNING: device at I2C address " + str(i) + " has not been identified as an EZO device, and will not be queried") 
             continue
         device_list.append(AtlasI2C(address = i, moduletype = moduletype, name = response))
     return device_list 
@@ -43,10 +47,6 @@ def get_devices():
 def main():
     
     device_list = get_devices()
-    
-    print(device_list)
-      
-    device = device_list[0]
     delaytime = 2
     
     try:
@@ -54,10 +54,14 @@ def main():
             dev.write("R")
         time.sleep(delaytime)
         for dev in device_list:
-            result = dev.read()
-            with open(f'dummy_data_{dev}.csv', 'a+') as dd:
-                dd.write(f'{result}\n')
-            print(result)
+            if dev.address != 100: # restrict operation to EC sensor for now
+                print(f'Not 100: {dev.address}')
+                continue
+            name = address_to_name[dev.address]
+            result = dev.read().strip().split(":")[-1].strip()
+            with open(f'data_{name}_address_{dev.address}.csv', 'a+') as dd:
+                dd.write(f'{datetime.now().strftime("%d-%m-%Y,%H:%M:%S")},{result}\n')                
+                print(dev.address, name, result)
         
     except KeyboardInterrupt:
         print("Continuous polling stopped")
